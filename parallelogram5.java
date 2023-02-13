b6bef850d1f06a24b05d82b348a066d02c095dfe
sahana780 <sahanakanva812001@gmail.com>
2023-02-13 17:57:16 +0530

    push4

---
+++ parallelogram5.java
@@ -0,0 +1,19 @@
+class Test8
+{
+static int area()
+{
+	int b=6;
+	int h=7;
+int c=b*h;
+return c;
+}
+}
+
+class parallelogram5
+{
+public static void main(String[] args)
+{
+ int x=Test8.area();
+ System.out.println(x);
+}
+}
